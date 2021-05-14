c9161813f78570ae88046e0fe4435429a86d4c3b
Phomint <patrick.dev.atom@gmail.com>
2021-05-14 11:05:46 -0300

    New query

---
+++ model/configure.py
@@ -0,0 +1,11 @@
+import json
+
+class Config:
+    def __init__(self):
+        pass
+
+    def load(self, path='../config.json'):
+        pass
+    def save(self, path='../config.json'):
+        pass
+        
